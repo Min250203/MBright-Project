c86bce498c0e889064b80a2dfd2e4b168cab93b6
antran2704 <phamtrangiaan27@gmail.com>
2024-04-16 20:49:30 +0700

    create helper

---
+++ helper/index.js
@@ -0,0 +1,151 @@
+const productHtml = (data) => {
+  return ` <div class="infor__product">
+        <div class="image__product">
+            <img class="item__iamge_product"
+                src='${ENDPOINT_IMAGE + data.thumbnail}'
+                alt="">
+        </div>
+        <div class="descr__product">
+            <div class="name__item">${data.title}</div>
+            <div class="descr__item">
+                <div class="price__item">${data.price}</div>
+            </div>
+            <button class="btn__buy_product">Mua Ngay</button>
+        </div>
+        </div>`;
+};
+
+const attributesHtml = (data) => {
+  const list = data.map((option) => {
+    return `<div class="size__item">${option.name}</div>
+        <div class="size__infor_product">
+            ${option.values.map((value) => {
+              return `<button class="select__size size1">${value.label}</button>`;
+            })}
+        </div>`;
+  });
+
+  return list.join(" ");
+};
+
+const productDetailHtml = (data) => {
+  return `
+            <div class="detail__infor_product">
+                <div class="name__infor_product">${data.title}</div>
+                <div class="price">${data.price} VND</div>
+                <div class="color__item">Màu</div>
+                <div class="color__infor_product">
+                    <div class="box__color color1">
+                        <div class="item__color" style="background-color: #000"></div>
+                    </div>
+                    <div class="box__color color2">
+                        <div class="item__color" style="background-color: #ccc"></div>
+                    </div>
+                    <div class="box__color color3">
+                        <div class="item__color" style="background-color: red"></div>
+                    </div>
+                    <div class="box__color color4">
+                        <div class="item__color" style="background-color: green"></div>
+                    </div>
+                </div>
+                <div class="size__item">Size</div>
+                <div class="size__infor_product">
+                    <button class="select__size size1">Size 1</button>
+                    <button class="select__size size2">Size 2</button>
+                    <button class="select__size size3">Size 3</button>
+                    <button class="select__size size4">Size 4</button>
+                </div>
+
+                <div class="quantity">
+                    <div class="name__quantity">Số lượng</div>
+                    <div class="action__quantity">
+                        <i class=" minus fa-solid fa-minus"></i>
+                        <span class="quantity__real">1</span>
+                        <i class="add fa-solid fa-plus"></i>
+                    </div>
+                </div>
+                <div class="btn__action">
+                    <button class="btn__buy_item">Mua Ngay</button>
+                    <button class="btn__add_item">Thêm vào giỏ hàng</button>
+                </div>
+                <div class="detail__des">${data.description}</div>
+            </div>`;
+};
+
+const renderListProduct = (items, parentEl) => {
+  let listHtml = items.map((item) => {
+    return productHtml(item);
+  });
+
+  listHtml = listHtml.join(" ");
+
+  if (parentEl) {
+    parentEl.innerHTML = listHtml;
+  }
+};
+
+const addEventProducts = (items) => {
+  const btnBuy = $$(".btn__buy_product");
+  const btnClose = $(".icon-close");
+  btnBuy.forEach((element, index) => {
+    element.onclick = function () {
+      console.log(element.parentNode.parentNode);
+      $(".container__overlay").classList.add("return__page");
+      $(".name__infor_product").textContent =
+        element.parentNode.querySelector(".name__item").textContent;
+      $(".price").textContent =
+        element.parentNode.querySelector(".price__item").textContent;
+      $(".mImage").src = element.parentNode.parentNode.querySelector(
+        ".item__iamge_product"
+      ).src;
+
+      const listImages = items[index].gallery.map((item) => {
+        return `<img class="dImage" src="${ENDPOINT_IMAGE + item}"
+            alt="">`;
+      });
+      $(".detail__image").innerHTML = listImages.join(" ");
+
+      const detailImage = $$(".dImage");
+
+      detailImage.forEach((element) => {
+        element.onclick = function (e) {
+          $(
+            ".mImg__product"
+          ).innerHTML = `<img class="mImage" src="${e.target.src}"
+                      alt="">`;
+        };
+      });
+
+      const attributeParentEl = $(".product_option");
+      if (items[index].options.length > 0) {
+        attributeParentEl.innerHTML = attributesHtml(items[index].options);
+      } else {
+        attributeParentEl.innerHTML = "";
+      }
+    };
+  });
+  btnClose.onclick = function () {
+    $(".container__overlay").classList.remove("return__page");
+    $(".quantity__real").textContent = `1`;
+    // btnSize.forEach((btn) => {
+    //   btn.classList.remove("change__color_select");
+    // });
+    // btnColor.forEach((e) => {
+    //   e.classList.remove("change__color_color");
+    // });
+  };
+};
+
+const fetchData = async (path, method, body = null) => {
+  const data = await fetch(ENDPOINT_SERVER + path, { method, body })
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
+
+  return data;
+};
+
+const convertToString = (payload) => {
+  return JSON.stringify(payload);
+};
+
+export { renderListProduct, fetchData, convertToString, addEventProducts };
--- product.js
+++ product.js
@@ -1,156 +1,198 @@
 import Cart from "./cart.js";
-const productItem = $('.all__product');
-const logo = $('.logo');
-const home = $('.home__page');
-const fortfolioProduct = $('.product__fortfolio');
-const btnBuy = $$('.btn__buy_product');
-const btnClose = $('.icon-close');
-const detailImage = $$('.dImage');
-const btnSize = $$('.select__size');
-const addProduct = $('.add');
-const minusProduct = $('.minus');
-const buyCart = $('.btn__buy_item')
-const addToCart = $('.btn__add_item');
-const btnColor = $$('.box__color')
+import {
+  renderListProduct,
+  fetchData,
+  addEventProducts,
+} from "./helper/index.js";
+
+const productItem = $(".all__product");
+const logo = $(".logo");
+const home = $(".home__page");
+const fortfolioProduct = $(".product__fortfolio");
+const btnBuy = $$(".btn__buy_product");
+const btnClose = $(".icon-close");
+const detailImage = $$(".dImage");
+const btnSize = $$(".select__size");
+const addProduct = $(".add");
+const minusProduct = $(".minus");
+const buyCart = $(".btn__buy_item");
+const addToCart = $(".btn__add_item");
+const btnColor = $$(".box__color");
+
+let listProduct = [];
 
 const Product = {
-    cart: [],
-    statusColor: '',
-    statusSize: 0,
-    quantityProduct: 1,
-    handleRenderProduct: function () {
-        let _this = this;
-        productItem.onclick = function () {
-            $('.slider').classList.add('change__product');
-            $('.content__pre_product').classList.add('change__product');
-            $('.container__content').classList.add('change__product');
-            $('.container__product_fortfolio').classList.add('change__product');
-            $('.container__product').classList.add('return__page');
-            $('.cart__product_order').classList.remove('return__page');
+  cart: [],
+  statusColor: "",
+  statusSize: 0,
+  quantityProduct: 1,
+  handleRenderProduct: function () {
+    let _this = this;
+    productItem.onclick = function () {
+      $(".slider").classList.add("change__product");
+      $(".content__pre_product").classList.add("change__product");
+      $(".container__content").classList.add("change__product");
+      $(".container__product_fortfolio").classList.add("change__product");
+      $(".container__product").classList.add("return__page");
+      $(".cart__product_order").classList.remove("return__page");
+    };
 
-
+    home.onclick = function () {
+      $(".slider").classList.remove("change__product");
+      $(".content__pre_product").classList.remove("change__product");
+      $(".container__content").classList.remove("change__product");
+      $(".container__product").classList.remove("return__page");
+      $(".container__product_fortfolio").classList.remove("return__page");
+      $(".cart__product_order").classList.remove("return__page");
+    };
+    logo.onclick = function () {
+      $(".slider").classList.remove("change__product");
+      $(".content__pre_product").classList.remove("change__product");
+      $(".container__content").classList.remove("change__product");
+      $(".container__product").classList.remove("return__page");
+      $(".container__product_fortfolio").classList.remove("return__page");
+      $(".cart__product_order").classList.remove("return__page");
+    };
+    btnBuy.forEach((element) => {
+      element.onclick = function () {
+        console.log(element.parentNode.parentNode);
+        $(".container__overlay").classList.add("return__page");
+        $(".name__infor_product").textContent =
+          element.parentNode.querySelector(".name__item").textContent;
+        $(".price").textContent =
+          element.parentNode.querySelector(".price__item").textContent;
+        $(".mImage").src = element.parentNode.parentNode.querySelector(
+          ".item__iamge_product"
+        ).src;
+      };
+    });
+    btnClose.onclick = function () {
+      $(".container__overlay").classList.remove("return__page");
+      $(".quantity__real").textContent = `1`;
+      btnSize.forEach((btn) => {
+        btn.classList.remove("change__color_select");
+      });
+      btnColor.forEach((e) => {
+        e.classList.remove("change__color_color");
+      });
+    };
+    detailImage.forEach((element) => {
+      element.onclick = function (e) {
+        $(
+          ".mImg__product"
+        ).innerHTML = `<img class="mImage" src="${e.target.src}"
+                    alt="">`;
+      };
+    });
+    btnSize.forEach((element) => {
+      element.onclick = function (e) {
+        btnSize.forEach((btn) => {
+          btn.classList.remove("change__color_select");
+        });
+        element.classList.add("change__color_select");
+        if (element.classList.contains("size1")) {
+          _this.statusSize = 1;
+        } else if (element.classList.contains("size2")) {
+          _this.statusSize = 2;
+        } else if (element.classList.contains("size3")) {
+          _this.statusSize = 3;
+        } else if (element.classList.contains("size4")) {
+          _this.statusSize = 4;
         }
+      };
+    });
+    addProduct.onclick = function () {
+      let quantity = Number($(".quantity__real").textContent);
+      console.log(quantity++);
+      $(".quantity__real").textContent = `${quantity++}`;
+      _this.quantityProduct = Number($(".quantity__real").textContent);
+    };
+    minusProduct.onclick = function () {
+      let quantity = Number($(".quantity__real").textContent);
+      console.log(quantity--);
 
-        home.onclick = function () {
-            $('.slider').classList.remove('change__product');
-            $('.content__pre_product').classList.remove('change__product');
-            $('.container__content').classList.remove('change__product');
-            $('.container__product').classList.remove('return__page')
-            $('.container__product_fortfolio').classList.remove('return__page');
-            $('.cart__product_order').classList.remove('return__page');
-
-
+      if (quantity >= 0) {
+        $(".quantity__real").textContent = `${quantity--}`;
+        _this.quantityProduct = Number($(".quantity__real").textContent);
+      } else {
+        console.log("hihih");
+      }
+    };
+    btnColor.forEach((item) => {
+      item.onclick = function () {
+        btnColor.forEach((e) => {
+          e.classList.remove("change__color_color");
+        });
+        item.classList.add("change__color_color");
+        if (item.classList.contains("color1")) {
+          _this.statusColor = "black";
+        } else if (item.classList.contains("color2")) {
+          _this.statusColor = "white";
+        } else if (item.classList.contains("color3")) {
+          _this.statusColor = "red";
+        } else if (item.classList.contains("color4")) {
+          _this.statusColor = "green";
         }
-        logo.onclick = function () {
-            $('.slider').classList.remove('change__product');
-            $('.content__pre_product').classList.remove('change__product');
-            $('.container__content').classList.remove('change__product');
-            $('.container__product').classList.remove('return__page');
-            $('.container__product_fortfolio').classList.remove('return__page');
-            $('.cart__product_order').classList.remove('return__page');
-
-
-        }
-        btnBuy.forEach((element) => {
-            element.onclick = function () {
-                console.log(element.parentNode.parentNode)
-                $('.container__overlay').classList.add('return__page')
-                $('.name__infor_product').textContent = element.parentNode.querySelector('.name__item').textContent
-                $('.price').textContent = element.parentNode.querySelector('.price__item').textContent
-                $('.mImage').src = element.parentNode.parentNode.querySelector('.item__iamge_product').src
-            }
-        })
-        btnClose.onclick = function () {
-            $('.container__overlay').classList.remove('return__page')
-            $(".quantity__real").textContent = `1`;
-            btnSize.forEach((btn) => {
-                btn.classList.remove('change__color_select')
-            })
-            btnColor.forEach((e) => {
-                e.classList.remove('change__color_color')
-            })
-        }
-        detailImage.forEach((element) => {
-            element.onclick = function (e) {
-                $('.mImg__product').innerHTML = `<img class="mImage" src="${e.target.src}"
-                    alt="">`
-            }
-        })
-        btnSize.forEach((element) => {
-            element.onclick = function (e) {
-                btnSize.forEach((btn) => {
-                    btn.classList.remove('change__color_select')
-                })
-                element.classList.add('change__color_select')
-                if (element.classList.contains('size1')) {
-                    _this.statusSize = 1
-                } else if (element.classList.contains('size2')) {
-                    _this.statusSize = 2
-                } else if (element.classList.contains('size3')) {
-                    _this.statusSize = 3
-                }
-                else if (element.classList.contains('size4')) {
-                    _this.statusSize = 4
-                }
-
-            }
-        })
-        addProduct.onclick = function () {
-            let quantity = Number($(".quantity__real").textContent)
-            console.log(quantity++)
-            $(".quantity__real").textContent = `${quantity++}`
-            _this.quantityProduct = Number($(".quantity__real").textContent)
-        }
-        minusProduct.onclick = function () {
-            let quantity = Number($(".quantity__real").textContent)
-            console.log(quantity--)
-
-            if (quantity >= 0) {
-                $(".quantity__real").textContent = `${quantity--}`
-                _this.quantityProduct = Number($(".quantity__real").textContent)
-            } else {
-                console.log("hihih")
-            }
-        }
-        btnColor.forEach((item) => {
-            item.onclick = function () {
-                btnColor.forEach((e) => {
-                    e.classList.remove('change__color_color')
-                })
-                item.classList.add("change__color_color")
-                if (item.classList.contains('color1')) {
-                    _this.statusColor = "black"
-                } else if (item.classList.contains('color2')) {
-                    _this.statusColor = 'white'
-                } else if (item.classList.contains('color3')) {
-                    _this.statusColor = 'red'
-                } else if (item.classList.contains('color4')) {
-                    _this.statusColor = 'green'
-                }
-            }
-        })
-        // buyCart.onclick = function () {
-        //     $('.slider').classList.add('change__product');
-        //     $('.content__pre_product').classList.add('change__product');
-        //     $('.container__content').classList.add('change__product');
-        //     $('.container__product_fortfolio').classList.remove('return__page')
-        //     $('.container__product').classList.remove('return__page');
-        //     $('.container__cart').classList.add('return__page');
-        //     $('.container__overlay').classList.remove('return__page')
-        // }
-        addToCart.onclick = function () {
-            console.log("hihiiiiiihihihihi")
-            console.log(_this.statusColor, "co", _this.statusSize, "size", _this.quantityProduct, "quan")
-            let nameProduct = $('.btn__add_item').parentNode.parentNode.querySelector('.name__infor_product').textContent;
-            let priceProduct = $('.btn__add_item').parentNode.parentNode.querySelector('.price').textContent;
-            let imgProduct = $('.btn__add_item').parentNode.parentNode.parentNode.querySelector('.mImage').src;
-            let product = { name: nameProduct, price: priceProduct, color: _this.statusColor, size: _this.statusSize, quantity: _this.quantityProduct, img: imgProduct}
-            _this.cart.push(product)
-            Cart.handleGoSeeOrder(_this.cart)
-        }
-
-    }
-
-}
+      };
+    });
+    // buyCart.onclick = function () {
+    //     $('.slider').classList.add('change__product');
+    //     $('.content__pre_product').classList.add('change__product');
+    //     $('.container__content').classList.add('change__product');
+    //     $('.container__product_fortfolio').classList.remove('return__page')
+    //     $('.container__product').classList.remove('return__page');
+    //     $('.container__cart').classList.add('return__page');
+    //     $('.container__overlay').classList.remove('return__page')
+    // }
+    addToCart.onclick = function () {
+      console.log("hihiiiiiihihihihi");
+      console.log(
+        _this.statusColor,
+        "co",
+        _this.statusSize,
+        "size",
+        _this.quantityProduct,
+        "quan"
+      );
+      let nameProduct = $(".btn__add_item").parentNode.parentNode.querySelector(
+        ".name__infor_product"
+      ).textContent;
+      let priceProduct =
+        $(".btn__add_item").parentNode.parentNode.querySelector(
+          ".price"
+        ).textContent;
+      let imgProduct =
+        $(".btn__add_item").parentNode.parentNode.parentNode.querySelector(
+          ".mImage"
+        ).src;
+      let product = {
+        name: nameProduct,
+        price: priceProduct,
+        color: _this.statusColor,
+        size: _this.statusSize,
+        quantity: _this.quantityProduct,
+        img: imgProduct,
+      };
+      _this.cart.push(product);
+      Cart.handleGoSeeOrder(_this.cart);
+    };
+  },
+};
 
 Product.handleRenderProduct();
+
+const getProducts = async () => {
+  const data = await fetchData("/products", "GET");
+
+  if (data.status === 200) {
+    const items = data.payload;
+    const parentElement = document.querySelector(
+      ".content__infor_product--first"
+    );
+    renderListProduct(items, parentElement);
+    addEventProducts(items);
+    listProduct = items;
+  }
+};
+
+getProducts();
